c5e8decf56574639f79278c90f1506c24bafb97c
matiasandrada333et20-jpg <matiasandrada333.et20@gmail.com>
2026-04-17 20:06:53 -0300

    SUBO PROYECTO

---
+++ TP3_EJERCICIO6/src/tp3_ejercicio6/TP3_EJERCICIO6.java
@@ -0,0 +1,46 @@
+package tp3_ejercicio6;
+
+import java.util.Scanner;
+
+public class TP3_EJERCICIO6 {
+
+    public static void main(String[] args) {
+
+        Scanner lector = new Scanner (System.in);
+        int N = 0;
+        int M = 0;
+    do{    
+        System.out.println("ingrese N y M ambos positivos");
+        N = lector.nextInt();
+        M = lector.nextInt();
+       
+        if(N >= M){
+            System.out.print("N debe ser menor a M");
+        }
+               
+
+    } while (M < N || N <= 0 || M <= 0);
+    for(int contador =1;contador<=M; contador++){
+       
+        if(contador% N == 0){
+           System.out.println("Multiplos:"+ contador);  
+        }
+    }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+ 
+ 
+
+     
+   
