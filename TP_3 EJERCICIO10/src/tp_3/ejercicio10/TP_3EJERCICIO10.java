c06debea65598d038ddaf51d4506edb27e447abd
matiasandrada333et20-jpg <matiasandrada333.et20@gmail.com>
2026-04-23 19:12:48 -0300

    EJERCICIO REALIZADO

--- TP_3 EJERCICIO10/src/tp_3/ejercicio10/TP_3EJERCICIO10.java
+++ TP_3 EJERCICIO10/src/tp_3/ejercicio10/TP_3EJERCICIO10.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package tp3_ejercicio10;
+package tp_3.ejercicio10;
 
 /**
  *
  * @author shefa
  */
-public class TP3_EJERCICIO10 {
+public class TP_3EJERCICIO10 {
 
     /**
      * @param args the command line arguments
